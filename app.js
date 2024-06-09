e8adfa483e48b80ec3bb06deda29a817473704f1
pawan0214 <148477911+pawan0214@users.noreply.github.com>
2024-06-09 10:27:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 # Hello-world-Node.js-app
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
 
 
